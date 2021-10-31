ecb410e4b6f31080428e1c1159da7dfb047cc9ee
김민재 <joshuara7235@gmail.com>
2021-10-31 22:26:58 +0900

    Test code for before lecture

---
+++ src/main/java/com/example/naverapi/test/Foo.java
@@ -0,0 +1,5 @@
+package com.example.naverapi.test;
+
+public class Foo extends Person{
+
+}
---
+++ src/main/java/com/example/naverapi/test/Person.java
@@ -0,0 +1,12 @@
+package com.example.naverapi.test;
+
+public class Person {
+    private String name;
+
+    public Person () {}
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+}
